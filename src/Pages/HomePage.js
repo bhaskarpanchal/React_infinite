d6ae38e541be36f0ed244548c03b46ff337fe541
BHASKAR PANCHAL <101353601+bhaskarpanchal@users.noreply.github.com>
2022-06-29 01:01:27 +0530

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,38 +1,61 @@
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Header from "../Components/Header";
+// import { render } from "react-dom";
 // import axios from "axios";
 
 function HomePage() {
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  const [Loader, setLoader] = useState(true);
+  let [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(true);
 
-  useEffect(() =>{
-    fetchData()
-  }, [])
+  useEffect(() => {
+    fetchData(page);
+  }, []);
 
-  function fetchData() {
-    fetch(`https://randomuser.me/api/?page=${page}&results=25&seed=abc`)
-      .then((res) => {
+  function fetchData(page) {
+    fetch(`https://randomuser.me/api/?page=${page}&results=10`)
+      
+    .then((res) => {
         if (res.ok) {
           return res.json();
         } else {
-          return Promise.reject({status: res.status,statusText: res.statusText});
+          return Promise.reject({
+            status: res.status,
+            statusText: res.statusText,
+          });
         }
       })
       .then((res) => {
-        setData(res.results);
+        if (page > 1) {
+          let resultAr = [...data, ...res.results];
+          setData(resultAr);
+        } else {
+          setData(res.results);
+        }
+        setLoader(false);
       })
       .catch((err) => console.log("Error, with message:", err.statusText));
   }
-
+  function loadMoreHandle(i) {
+    console.log(i);
+    let bottom =
+      i.target.scrollHeight - i.target.clientHeight - i.target.scrollTob < 10;
+    if (bottom) {
+      let page_ = page + 1;
+      fetchData(page_);
+      setLoader(true);
+      setPage(page_);
+    }
+  }
   return (
-    <div>
+    <div onScroll={loadMoreHandle} className="table-wrap">
       <table class="table">
         <thead>
           <tr>
-            <th scope="col">name</th>
-            <th scope="col">gender</th>
-            <th scope="col">profile pic</th>
+            <th scope="col">Name</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Profile pic</th>
           </tr>
         </thead>
         <tbody>
@@ -42,14 +65,21 @@ function HomePage() {
                 <td>{itm.name["first"]}</td>
                 <td>{itm.gender}</td>
                 <td>
-                  <img src = {itm.picture["thumbnail"]}/>
+                  <img src={itm.picture["thumbnail"]} />
                 </td>
               </tr>
             );
           })}
+          <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+              <span class="sr-only"></span>
+            </div>
+          </div>
         </tbody>
       </table>
     </div>
   );
 }
+
+// render(<HomePage />, document.getElementById("root"));
 export default HomePage;
